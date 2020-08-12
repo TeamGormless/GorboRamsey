452017caa4ca4f54ff10b3413d489f019152f5da
Jamie Lewis <TeamGormless@gmail.com>
2020-08-12 14:55:11 +0100

    Preliminary work on Machines

---
+++ Assets/Scripts/Machines/Base/MachineInputPort.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineInputPort : MonoBehaviour
+{
+    public delegate void InputEvent(PickupableObject pickupableObject);
+    public event InputEvent OnObjectInput;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PickupableObject p = other.GetComponent<PickupableObject>();
+        if (p)
+            OnObjectInput?.Invoke(p);
+    }
+}
